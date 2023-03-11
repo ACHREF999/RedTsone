14fa858e4cb02eb1da3781315bae0715ec8a62cd
ACHREF999 <achrefdouda1@gmail.com>
2023-03-12 00:19:12 +0100

    adding some UI no linking so far

---
+++ gui.py
@@ -0,0 +1,23 @@
+from PyQt5 import QtWidgets,uic
+from PyQt5.QtWidgets import QApplication,QMainWindow
+import sys
+
+
+# from .UI.ui_FirstStacking import Ui_MainWindow
+from UI import Ui_MainWindow
+
+# class UI(QMainWindow):
+#     def __init__(self):
+#         super(UI,self).__init__()
+#         uic.loadUi('./UI/FirstStacking.ui',self)
+#         self.show()
+class Window(Ui_MainWindow,QMainWindow):
+    def __init__(self):
+        super(Window,self).__init__()
+        self.setupUi(self)
+
+app = QApplication(sys.argv)
+print('asdasdasdsad')
+win = Window()
+win.show()
+app.exec()
